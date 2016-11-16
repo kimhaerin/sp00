af140e3c64d487f5f6904fa850809266821ae27e
해린 <@haerin>
2016-11-16 12:22:07 +0900

    20161116

--- src/main/java/org/zerock/web/SmemControllerTest.java
+++ src/main/java/org/zerock/web/SmemControllerTest.java
@@ -3,11 +3,14 @@ package org.zerock.web;
 import javax.inject.Inject;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 @WebAppConfiguration
@@ -15,15 +18,23 @@ import org.springframework.web.context.WebApplicationContext;
 @ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/**-context.xml" })
 
 public class SmemControllerTest {
-	
+
 	@Inject
 	private WebApplicationContext ctx;
-	
+
 	private MockMvc mockMVC;
-	
+
 	@Before
-	public void setup(){
-		System.out.println("왜 안되니??");
+	public void setup() {
+		this.mockMVC = MockMvcBuilders.webAppContextSetup(ctx).build();
 	}
 
+	@Test
+	public void testDoA() throws Exception {
+		mockMVC.perform(MockMvcRequestBuilders
+				.post("/smem/doA")
+				.param("userid", "AAAA")
+				.param("userpw", "BBBB"));
+	}
+	
 }
